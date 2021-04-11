e68ca65a5d4be4b9c213fefba1eb9d27e3e53803
Shriyans Gandhi <shri30yans@gmail.com>
2021-04-11 12:26:36 +0530

    dingding update

--- config.py
+++ config.py
@@ -27,8 +27,11 @@ events_channel_id=748786284599705688
 starboard_channel_id=788600643925180426
 #starboard_channel_id=818497930545594408
 
-upvote_reaction="<:Logo_Reddit_Upvote:748810439885979718>"
-downvote_reaction="<:Logo_Reddit_downvote:748810439688716328>"
+# upvote_reaction="<:Logo_Reddit_Upvote:748810439885979718>"
+# downvote_reaction="<:Logo_Reddit_downvote:748810439688716328>"
+upvote_reaction="<:dingding:830695446821339137>"
+downvote_reaction="<:dongdong:830697221959450656>"
+
 reddit_award_ternion="<:reddit_award_ternion:809384449413742633>"
 reddit_award_argentinum="<:reddit_award_argentinum:818740567734878248>" 
 reddit_award_platinum="<:reddit_award_platinum:809383626085105694>"
