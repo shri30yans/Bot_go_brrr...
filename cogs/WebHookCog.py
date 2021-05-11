23da6d8005f2d6750badad1527744cebcf6a0441
Shriyans Gandhi <shri30yans@gmail.com>
2021-05-11 13:00:10 +0530

    Emoji Webhook Update

---
+++ cogs/WebHookCog.py
@@ -0,0 +1,53 @@
+import discord,random,aiohttp,json,asyncio
+from discord.ext import commands,tasks
+# import utils.awards as awards
+# import config   
+from discord import Webhook, AsyncWebhookAdapter
+import aiohttp
+    
+class WebHook(commands.Cog): 
+    def __init__(self, bot):
+        self.bot = bot
+
+    
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        print(message.content)
+        if message.content.startswith(";") and message.content.endswith(";"):
+            await message.delete()
+            await self.send_message(message)
+
+        
+           
+    async def send_message(self,message):
+        user=message.author
+        url = await self.get_webhook_url(message)
+        emoji=await self.get_emoji(message)
+        async with aiohttp.ClientSession() as session:
+            webhook = Webhook.from_url(url, adapter=AsyncWebhookAdapter(session))
+            await webhook.send(emoji, username=user.display_name,avatar_url=user.avatar_url)
+            #await webhook.send('Hello World', username=user.name,avatar_url=user.avatar_url)
+
+    async def get_webhook_url(self,message):
+        channel=message.channel
+        webhook_urls= await channel.webhooks()
+        print(webhook_urls)
+        for webhook in webhook_urls:
+            if webhook.name=="Emoji Webhook":
+                return webhook.url
+            else:
+                webhook=await channel.create_webhook(name="Emoji Webhook", avatar=None, reason="Webhook to use animated emojis.")
+                return webhook.url
+    
+    async def get_emoji(self,message):
+        emoji_name = message.content.lower()[1:-1]
+        for emoji in self.bot.emojis:
+            if emoji_name == emoji.name.lower():
+                return emoji
+
+            
+
+    
+
+def setup(bot):
+    bot.add_cog(WebHook(bot))
